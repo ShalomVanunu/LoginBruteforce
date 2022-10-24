301cde622896ce3a4936254c8079ae80178f2abb
shalomvanunu <shalomvanunu@gmail.com>
2022-10-24 21:24:55 +0300

    Updated 24/10/22

--- LoginPage.py
+++ LoginPage.py
@@ -39,4 +39,4 @@ def register():
     return render_template('register.html' , msg='')
 
 #if __name__ == "__main__":
-#    app.run(debug=True, host="0.0.0.0" , port=80 )
+#   app.run(debug=True, host="0.0.0.0" , port=80 )
